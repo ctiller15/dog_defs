57e940bfb3b81a16ae1d6446d12022bfac21768e
Christopher Tiller <christophertiller@ChristophersMBP.lan>
2024-04-03 18:36:41 -0400

    initialize supabase, initialize base template.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 - A project to create and share definitions for any and all dog-related terminology.
 
 ## Running the container
+
 ```bash
+# macos m1+
+docker buildx build --platform linux/amd64 -t dog-defs .
+docker run -p 8080:8080 dog-defs
+
 docker build -t dog-defs .
 docker run -p 8080:8080 dog-defs
 ```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +9,13 @@ import (
 
 func homePage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "Welcome to dogdefs!",
+		"title":    "DogDefs",
+		"greeting": "Welcome to dogdefs!",
 	})
+}
+
+func saveNewDefinition(c *gin.Context) {
+	fmt.Println("Saving definition!")
 }
 
 func main() {
@@ -17,6 +23,8 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html") // To load individual html files
 	r.GET("/", homePage)
+
+	r.POST("/definitions/new", saveNewDefinition)
 
 	r.Run(":8080")
 }
