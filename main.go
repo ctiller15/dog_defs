5b3180e56d95f968684ed225033c8184acdf4410
Christopher Tiller <christophertiller@ChristophersMBP.lan>
2024-04-03 20:51:41 -0400

    add basic templates for words and definitions routes

--- main.go
+++ main.go
@@ -14,8 +14,33 @@ func homePage(c *gin.Context) {
 	})
 }
 
+func wordsPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "words.html", gin.H{
+		"title": "Words",
+	})
+}
+
+func newDefinitionsPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "new_definition.html", gin.H{
+		"title": "New Definition",
+	})
+}
+
+type newDefinitionForm struct {
+	Word         string `form:"word"`
+	PartOfSpeech string `form:"pos"`
+	Definition   string `form:"definition"`
+	Reference    string `form:"reference"`
+}
+
 func saveNewDefinition(c *gin.Context) {
 	fmt.Println("Saving definition!")
+	var newForm newDefinitionForm
+	c.ShouldBind(&newForm)
+	c.HTML(http.StatusOK, "definition_saved.html", gin.H{
+		"title": "Definition Saved",
+		"word":  newForm.Word,
+	})
 }
 
 func main() {
@@ -23,7 +48,9 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html") // To load individual html files
 	r.GET("/", homePage)
+	r.GET("/words", wordsPage)
 
+	r.GET("/definitions/new", newDefinitionsPage)
 	r.POST("/definitions/new", saveNewDefinition)
 
 	r.Run(":8080")
